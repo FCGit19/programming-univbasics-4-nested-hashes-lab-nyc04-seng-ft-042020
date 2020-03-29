c48314718b7939b6a2d54d2bd68e05ba3de8f5d4
Fred C <canofred@gmail.com>
2020-03-29 04:07:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -61,14 +61,16 @@ end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#   :languages => ["LISP", "C"]
-# }
+
 # return the entire updated hash
 
 	programmer_hash = 
  		{
+ 		  :yukihiro_matsumoto => {
+        :known_for => "Ruby",
+        :languages => ["LISP", "C"]
+      },
+ 		  
       :grace_hopper => {
         :known_for => "COBOL",
         :languages => ["COBOL", "FORTRAN"]
@@ -82,8 +84,11 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+programmer_hash
 end
+
+
+
 
 def changing_alan
   # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
